a375de9c5097d0bbb71892d7afb8d6e73e9c2ce1
Joe Fiorini <joe@joefiorini.com>
2012-12-13 17:21:07 -0500

    Generate references in models

--- lib/generators/nifty/scaffold/templates/model.rb
+++ lib/generators/nifty/scaffold/templates/model.rb
@@ -1,4 +1,8 @@
 class <%= class_name %> < ActiveRecord::Base
 <%= "  set_table_name :#{table_name}\n" if table_name -%>
   attr_accessible <%= model_attributes.map { |a| ":#{a.name}" }.join(", ") %>
+<% model_attributes.select {|attr| attr.reference? }.each do |attribute| -%>
+  belongs_to :<%= attribute.name %>
+<% end -%>
+
 end
