c8a345c3dcd8eb3203e4f893e35c29e952af3c9c
Joe Fiorini <joe@joefiorini.com>
2012-12-13 17:20:52 -0500

    Use Rails 3 change migration

--- lib/generators/nifty/scaffold/templates/migration.rb
+++ lib/generators/nifty/scaffold/templates/migration.rb
@@ -1,5 +1,5 @@
 class Create<%= class_name.pluralize.delete('::') %> < ActiveRecord::Migration
-  def self.up
+  def change
     create_table :<%= table_name || plural_name.split('/').last %> do |t|
     <%- for attribute in model_attributes -%>
       t.<%= attribute.type %> :<%= attribute.name %>
@@ -8,9 +8,5 @@ class Create<%= class_name.pluralize.delete('::') %> < ActiveRecord::Migration
       t.timestamps
     <%- end -%>
     end
-  end
-
-  def self.down
-    drop_table :<%= table_name || plural_name.split('/').last %>
   end
 end
